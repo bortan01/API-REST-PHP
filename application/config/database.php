592cee9793f4f149d4ad66a58d941df6f29eb47a
Boris Ricardo Miranda <brmiranda009@gmail.com>
2020-07-11 22:19:27 -0600

    dejandolo bien

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'restful_db',
+	'hostname' => 'www.christianmeza.com',
+	'username' => 'christi5_prueba',
+	'password' => '@L&-vbWzg]MW',
+	'database' => 'christi5_otrabd',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
