38c210fbd87f055c11a724ce950dcdc8bd52ec98
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-10-01 15:43:57 -0600

    detalle cita

--- application/controllers/Cita.php
+++ application/controllers/Cita.php
@@ -9,10 +9,8 @@ public function __construct(){
 		parent::__construct();
 		$this->load->database();
 		$this->load->model('Cita_model');
-		//$this->load->model('Pregunta_model');
-		//$this->load->helper('utilidades');
-
-	}
+	
+		}
 
 public function cita_get(){
 
@@ -40,6 +38,45 @@ public function cita_get(){
 	public function citas_post(){
 
 		$data=$this->post();
+		$this->load->library('form_validation');
+		$this->form_validation->set_data($data);
+
+		if ( $this->form_validation->run('citas_put') ) {
+			//todo bien
+			//$this->response('Todo bien');
+		$cita=$this->Cita_model->set_datos($data);
+
+		$respuesta=$this->Cita_model->insertCita($cita); 
+
+		if ($respuesta['err']) {
+
+		$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST); 	
+
+		}else{
+		$this->response($respuesta); 	
+		}
+
+		}else{
+			//algo mal
+
+			$respuesta=array(
+				'err'=>TRUE,
+				'mensaje'=>'Hay errores en el envio de la informacion',
+				'errores'=>$this->form_validation->get_errores_arreglo()
+			);
+			$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST); 
+		}
+
+	}
+
+		
+
+}
+
+/*
+prueba
+
+$data=$this->post();
 		$this->load->library('form_validation');
 		$this->form_validation->set_data ($data);
 
@@ -74,4 +111,5 @@ public function cita_get(){
 		}
 	}
 
-}
+
+*/
--- application/models/Cita_model.php
+++ application/models/Cita_model.php
@@ -3,12 +3,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Cita_model extends CI_Model
 {
 public $id_cita;
+public $id_cliente;
 public $descripcion;
 public $title;
 public $color;
 public $textColor;
 public $start;
-public $end;
+public $fecha;
+public $hora;
+
 
 public function get_citas(){
 
@@ -18,30 +21,26 @@ public function get_citas(){
  		return $query->result();
  	}
 
-    public function set_datos( $data_cruda){
+    public function set_datos($data_cruda){
+    	 $objeto =array();
+        ///par aquitar campos no existentes
+        foreach ($data_cruda as $nombre_campo => $valor_campo) {
+            # para verificar si la propiedad existe..
+            if (property_exists('Cita_model', $nombre_campo)) {
+                $objeto[$nombre_campo] = $valor_campo;
+            }
+        }
 
- 		foreach ($data_cruda as $nombre_campo => $valor_campo) {
-
- 		if (property_exists('cita_model',$nombre_campo)) {
- 			$this->$nombre_campo=$valor_campo;
- 		
- 		}
- 			
- 		}
- 		return $this; //retornamos el objeto de clase
+        return $objeto;
  	}//fin de capitalizar los datos segun el modelo y campos correctos de la base
 
- 	public function insertCita($title,$descripcion,$start,$fecha){
+ 	public function insertCita($datos){
 
 			//insertar el registro
-			$this->title =$title;
-			$this->descripcion=$descripcion;
-			$this->start=$fecha.' '.$start;
-			$this->textColor="#FFFFFF";
-			$this->color="#007bff";
-
-
-			$hecho=$this->db->insert('cita',$this);
+			
+			//$this->textColor="#FFFFFF";
+			//$this->color="#007bff";
+			$hecho=$this->db->insert('cita',$datos);
 
 			if ($hecho) {
 				#insertado
@@ -49,11 +48,8 @@ public function get_citas(){
 					'err'=>FALSE,
 					'mensaje'=>'Registro insertado correctamente',
 					'cita_id'=>$this->db->insert_id(),
-					'ver'=>$this
+					'ver'=>$datos
 				);
-
-			
-
 			}else{
 				//error
 
