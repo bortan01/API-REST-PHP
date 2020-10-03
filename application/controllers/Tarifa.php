1c283b5e76910e38b565c395d9eaaea31219719e
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-10-02 21:04:13 -0600

    tarifaInsert

--- application/controllers/Tarifa.php
+++ application/controllers/Tarifa.php
@@ -11,4 +11,35 @@ public function __construct(){
 		$this->load->model('Tarifa_model');
 
 	}
+public function Tarifa_post(){
+
+	    $data=$this->post();
+		$this->load->library('form_validation');
+		$this->form_validation->set_data($data);
+
+		if ( $this->form_validation->run('tarifa_put') ) {
+			//todo bien
+			//$this->response('Todo bien');
+		$tarifa=$this->Tarifa_model->set_datos($data);
+        $respuesta=$this->Tarifa_model->insert($tarifa); 
+
+		if ($respuesta['err']) {
+
+		$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST); 	
+
+		}else{
+		$this->response($respuesta); 	
+		}
+
+		}else{
+			//algo mal
+
+			$respuesta=array(
+				'err'=>TRUE,
+				'mensaje'=>'Hay errores en el envio de la informacion',
+				'errores'=>$this->form_validation->get_errores_arreglo()
+			);
+			$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST); 
+		}
+}//fin metodo
 }
--- application/models/Tarifa_model.php
+++ application/models/Tarifa_model.php
@@ -2,5 +2,56 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Tarifa_model extends CI_Model
 {
+	public $id_tarifa;
+	public $libras;
+	public $tarifa;
+	public $comision;
+
+	public function set_datos($data_cruda){
+    	 $objeto =array();
+        ///par aquitar campos no existentes
+        foreach ($data_cruda as $nombre_campo => $valor_campo) {
+            # para verificar si la propiedad existe..
+            if (property_exists('Tarifa_model', $nombre_campo)) {
+                $objeto[$nombre_campo] = $valor_campo;
+            }
+        }
+
+        return $objeto;
+ 	}//fin de capitalizar los datos segun el modelo y campos correctos de la base
+
+ 	public function insert($datos){
+
+
+			//insertar el registro
+			$hecho=$this->db->insert('tarifa',$datos);
+
+			if ($hecho) {
+				#insertado
+				$respuesta=array(
+					'err'=>FALSE,
+					'mensaje'=>'Registro insertado correctamente',
+					'tarifa_id'=>$this->db->insert_id(),
+					'datos'=>$datos
+				);
+
+			
+
+			}else{
+				//error
+
+				$respuesta=array(
+					'err'=>TRUE,
+					'mensaje'=>'Error al insertar',
+					'error'=>$this->db->_error_message(),
+					'error_num'=>$this->db->_error_number()
+				);
+			
+			}
+
+
+
+ 		return $respuesta;
+ 	}//fin de insertar
 
 }
