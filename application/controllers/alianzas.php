d6c5e7dbe5ff43cf46effc8241e23c7a69644701
Abigail <pineverdi@gmail.com>
2020-10-05 15:17:30 -0600

    Modificar de Alianza (vuelos)

--- application/controllers/alianzas.php
+++ application/controllers/alianzas.php
@@ -33,13 +33,11 @@ public function __construct(){
 		$this->response($respuesta,REST_Controller::HTTP_NOT_FOUND);
 
 	}
-}
-
-	
+}	
 	//INSERTAR
-	public function alianzas_put(){
+	public function alianzas_post(){
 
-		$data=$this->put();
+		$data=$this->post();
 		$this->load->library('form_validation');
 		$this->form_validation->set_data ($data);
 
@@ -65,5 +63,27 @@ public function __construct(){
 			);
 			$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST); 
 		}
+	}
+
+	//MODIFICAR
+	public function actualizarAlianza_put(){
+
+		$data = $this->put();
+        if (!isset($data["idalianza"])) {
+            $respuesta = array('err' => TRUE, 'mensaje' => 'No se encontro ningun identificador de Alianza');
+            $this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            try {
+                $respuesta = $this->Alianzas_model->editar($data);
+                if ($respuesta['err']) {
+                    $this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+                } else {
+                    $this->response($respuesta, REST_Controller::HTTP_OK);
+                }
+            } catch (\Throwable $th) {
+                $respuesta = array('err' => TRUE, 'mensaje' => 'Error interno de servidor');
+            }
+        }
+
 	}
 }
--- application/models/Alianzas_model.php
+++ application/models/Alianzas_model.php
@@ -11,24 +11,19 @@ class Alianzas_model extends CI_Model
 
             return $query->result();
         }
-   
-   
        public function set_datos( $data_cruda){
-   
             foreach ($data_cruda as $nombre_campo => $valor_campo) {
    
             if (property_exists('Alianzas_model',$nombre_campo)) {
                 $this->$nombre_campo=$valor_campo;
             
             }
-                
             }
             return $this; 
         }
    
         public function insert(){
    
-            
            $query=$this->db->get_where('alianza',array('nombre_alianza'=>$this->nombre_alianza) );
            $alianzas=$query->row();
    
@@ -50,24 +45,64 @@ class Alianzas_model extends CI_Model
                        'mensaje'=>'Registro insertado correctamente',
                        'alianza_id'=>$this->db->insert_id()
                    );
-   
-               
-   
                }else{
                    //error
-   
                    $respuesta=array(
                        'err'=>TRUE,
                        'mensaje'=>'Error al insertar',
                        'error'=>$this->db->_error_message(),
                        'error_num'=>$this->db->_error_number()
                    );
-               
                }
-   
-   
-   
             return $respuesta;
         }
+    //MODIFICAR
+    public function editar($data)
+    {
+        $nombreTabla = "alianza";
+
+        ///VAMOS A ACTUALIZAR UN REGISTRO
+        $campos = $this->Alianzas_model->verificar_camposEntrada($data);
+        $this->db->where('idalianza', $campos["idalianza"]);
+
+        $hecho = $this->db->update($nombreTabla, $campos);
+        if ($hecho) 
+        {
+            ///LOGRO ACTUALIZAR 
+            $respuesta = array(
+                'err'     => FALSE,
+                'mensaje' => 'Registro Actualizado Exitosamente',
+                'Alianza' => $campos
+
+            );
+            return $respuesta;
+        } 
+        else 
+        {
+            //NO GUARDO
+            $respuesta = array(
+                'err' => TRUE,
+                'mensaje' => 'Error al actualizar ', $this->db->error_message(),
+                'error_number' => $this->db->error_number(),
+                'Alianza' => null
+            );
+            return $respuesta;
+        }
+    }
+
+    //VERIFICAR DATOS
+    public function verificar_camposEntrada($dataCruda)
+    {
+        $objeto = array();
+        ///quitar campos no existentes
+        foreach ($dataCruda as $nombre_campo => $valor_campo) {
+            # para verificar si la propiedad existe..
+            if (property_exists('Alianzas_model', $nombre_campo)) {
+                $objeto[$nombre_campo] = $valor_campo;
+            }
+        }
+        return $objeto;
+    }
+   
    
 }
