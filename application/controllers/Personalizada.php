27ae69c45e86b77c2eb9b847c167f37abda4efbc
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-11-24 22:03:16 -0600

    modificacion modelo

---
+++ application/controllers/Personalizada.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+class Personalizada extends REST_Controller
+{
+
+public function __construct(){
+		//constructor del padre
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('Personalizada_model');
+	}
+
+//******registro de forma de operar estandar
+public function puntoReferenciaPer_get(){
+	$data = $this->get();
+	$punto = $this->Personalizada_model->get_puntoPersonalizada($data);
+
+	if (count($punto)>0) {
+		
+		$respuesta=array(
+			'err'=>FALSE,
+			'mensaje'=>'Registro Cargado correctamente',
+			'punto'=>$punto
+
+		);
+		$this->response($respuesta,REST_Controller::HTTP_OK);
+	}else{
+		$respuesta=array(
+			'err'=>TRUE,
+			'mensaje'=>'No hay ruta para ',
+			'punto'=>null
+
+		);
+		$this->response($respuesta,REST_Controller::HTTP_NOT_FOUND);
+
+	}
+}
+
+public function productosCombo_get(){
+	$data = $this->get();
+	$punto = $this->Personalizada_model->get_productosCombo($data);
+
+	if (count($punto)>0) {
+		
+		$respuesta=array(
+			'err'=>FALSE,
+			'mensaje'=>'Registro Cargado correctamente',
+			'punto'=>$punto
+
+		);
+		$this->response($respuesta,REST_Controller::HTTP_OK);
+	}else{
+		$respuesta=array(
+			'err'=>TRUE,
+			'mensaje'=>'No hay ruta para ',
+			'punto'=>null
+
+		);
+		$this->response($respuesta,REST_Controller::HTTP_NOT_FOUND);
+
+	}
+}
+
+public function productosCosto_get(){
+	$data = $this->get();
+	$punto = $this->Personalizada_model->get_productosCosto($data);
+
+	if (count($punto)>0) {
+		
+		$respuesta=array(
+			'err'=>FALSE,
+			'mensaje'=>'Registro Cargado correctamente',
+			'punto'=>$punto
+
+		);
+		$this->response($respuesta,REST_Controller::HTTP_OK);
+	}else{
+		$respuesta=array(
+			'err'=>TRUE,
+			'mensaje'=>'No hay ruta para ',
+			'punto'=>null
+
+		);
+		$this->response($respuesta,REST_Controller::HTTP_NOT_FOUND);
+
+	}
+}
+
+
+}
--- application/models/Personalizada_model.php
+++ application/models/Personalizada_model.php
@@ -10,6 +10,57 @@ class Personalizada_model extends CI_Model
 	public $id_municipios;
 	public $id_empresa;
 
+
+	public function get_productosCosto(array $data){
+
+		$query=$this->db->get_where('empresa',array('nombre_empresa'=>$data['nombre_empresa']) );
+		$id_empresa=$query->row('id_empresa');
+
+        $this->db->select('*');
+        $this->db->from('personalizada');
+        $this->db->join('producto', 'producto.id_producto=personalizada.id_producto','inner');
+        $this->db->where(array('personalizada.id_empresa'=>$id_empresa,'personalizada.id_producto'=>$data['id_producto']));
+        $query = $this->db->get();
+
+        //$query=$this->db->get('vehiculo');
+
+        return $query->result();
+
+}
+
+	public function get_productosCombo(array $data){
+
+		$query=$this->db->get_where('empresa',array('nombre_empresa'=>$data['nombre_empresa']) );
+		$id_empresa=$query->row('id_empresa');
+
+        $this->db->select('*');
+        $this->db->from('personalizada');
+        $this->db->join('producto', 'producto.id_producto=personalizada.id_producto','inner');
+        $this->db->where(array('id_empresa'=>$id_empresa,'id_municipios'=>$data['id_municipios']));
+        $query = $this->db->get();
+
+        //$query=$this->db->get('vehiculo');
+
+        return $query->result();
+
+}
+
+	public function get_puntoPersonalizada(array $data){
+
+		$query=$this->db->get_where('empresa',array('nombre_empresa'=>$data['nombre_empresa']) );
+		$id_empresa=$query->row('id_empresa');
+
+        $this->db->select('*');
+        $this->db->from('personalizada');
+        $this->db->where(array('id_empresa'=>$id_empresa,'id_municipios'=>$data['id_municipios']));
+        $query = $this->db->get();
+
+        //$query=$this->db->get('vehiculo');
+
+        return $query->result();
+
+}
+
 	public function insertarPersonalizada($id_producto,$datos){
 
 		//extraer la empresa
