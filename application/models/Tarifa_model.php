e47262dc315c82a9ad4369c4ff7a9f9cc15b2eda
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-03-20 23:32:09 -0600

    para insertar la tarifa

--- application/models/Tarifa_model.php
+++ application/models/Tarifa_model.php
@@ -4,10 +4,10 @@ class Tarifa_model extends CI_Model
 {
 	public $id_tarifa;
 	public $tarifa;
-	public $unidades_medidas;
+	public $id_unidad_medida;
 	public $id_producto;
 
-	public function eliminar($datos){
+public function eliminar($datos){
 
 		$query=$this->db->get_where('tarifa',array('id_tarifa'=>$datos["id_tarifa"]) );
 		$ta=$query->row();
@@ -42,10 +42,9 @@ class Tarifa_model extends CI_Model
 			
 			}
  		return $respuesta;
-	}//fin metodo
+}//fin metodo
 
-
-	public function modificar_tarifa($datos){
+public function modificar_tarifa($datos){
 		$this->db->set(array('tarifa'=>$datos['tarifa'],
 			                 'unidades_medidas'=>$datos['unidades_medidas']
 	                  ));
@@ -75,9 +74,9 @@ class Tarifa_model extends CI_Model
 			}
 
  		return $respuesta;
- 	}//fin metodo
+}//fin metodo
 
-	public function get_tarifa($data){
+public function get_tarifa($data){
 
  	    $this->db->select('*');
         $this->db->from('tarifa');
@@ -88,10 +87,11 @@ class Tarifa_model extends CI_Model
  		return $query->result();
  	}
 
-	public function set_datos($data_cruda){
+public function set_datos($data_cruda){
     	 $objeto =array();
         ///par aquitar campos no existentes
-        foreach ($data_cruda as $nombre_campo => $valor_campo) {
+        foreach ($data_cruda as $nombre_campo => $valor_campo)
+        {
             # para verificar si la propiedad existe..
             if (property_exists('Tarifa_model', $nombre_campo)) {
                 $objeto[$nombre_campo] = $valor_campo;
@@ -99,12 +99,12 @@ class Tarifa_model extends CI_Model
         }
 
         return $objeto;
- 	}//fin de capitalizar los datos segun el modelo y campos correctos de la base
+}//fin de capitalizar los datos segun el modelo y campos correctos de la base
 
- 	public function insertarTarifa($id_producto,$datos){
+public function insertarTarifa($id_producto,$datos){
 
  			$this->tarifa = $datos['tarifa'];
- 			$this->unidades_medidas = $datos['unidades_medidas'];
+ 			$this->id_unidad_medida = $datos['id_unidad'];
  			$this->id_producto = $id_producto;
 			//insertar el registro
 			$hecho=$this->db->insert('tarifa',$this);
@@ -135,6 +135,6 @@ class Tarifa_model extends CI_Model
 
 
  		return $respuesta;
- 	}//fin de insertar
+}//fin de insertar
 
 }
