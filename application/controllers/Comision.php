ae5d05965449d13f5609a13586ef48b9695b0513
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-07-04 11:26:21 -0600

    agregando update comision

--- application/controllers/Comision.php
+++ application/controllers/Comision.php
@@ -23,5 +23,20 @@ class Comision extends REST_Controller
 
     }//fin
 
+    public function comisionUpdate_post(){
+    	$data=$this->post();
+
+		$respuesta=$this->Comision_model->updateComision($data);
+
+		if ($respuesta['err']) {
+		$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST); 	
+
+		}else{
+		$this->response($respuesta); 	
+		}
+
+    }//fin
+
+
 
 }
--- application/models/Comision_model.php
+++ application/models/Comision_model.php
@@ -5,18 +5,18 @@ class Comision_model extends CI_Model
 	public $id_comision;
 	public $porcentaje;
 
-	public function insertarComisionAuto(){
-		$this->db->select('porcentaje');
+public function insertarComisionAuto(){
+         $this->db->select('porcentaje');
         $this->db->from('comision');
         $comision=$this->db->get();
          $row = $comision->row('porcentaje');
         if ($row > 0) {
                 $respuesta=array(
-					'err'=>TRUE,
-					'mensaje'=>'Ya esta la comision'
-					);
+		'err'=>TRUE,
+	        'mensaje'=>'Ya esta la comision'
+		 );
                 
-			return $respuesta;
+	return $respuesta;
         }else{
 
         	
@@ -24,12 +24,47 @@ class Comision_model extends CI_Model
 		$hecho=$this->db->insert('comision',$this);
 
 		if ($hecho) {
-			$respuesta=array(
-					'err'=>FALSE,
-					'mensaje'=>'Registro insertado correctamente'
-				);
+		$respuesta=array(
+		       'err'=>FALSE,
+			'mensaje'=>'Registro insertado correctamente'
+			);
 		}
 		return $respuesta;
 	   }
-	}
+}
+
+public function updateComision($datos){
+	///para modificar
+ 		
+ 	$this->db->set(array('porcentaje'=> $datos['porcentaje']));
+ 	$this->db->where('id_comision',1);
+        $hecho=$this->db->update('comision');
+        
+			if ($hecho) {
+				#insertado
+				$respuesta=array(
+					'err'=>FALSE,
+					'mensaje'=>'Registro Actualizado correctamente',
+					'producto_id'=>$this->db->insert_id(),
+					'datos'=>$datos
+				);
+
+			
+
+			}else{
+				//error
+
+				$respuesta=array(
+					'err'=>TRUE,
+					'mensaje'=>'Error al insertar',
+					'error'=>$this->db->_error_message(),
+					'error_num'=>$this->db->_error_number()
+				);
+			
+			}
+
+			return $respuesta;
+
+}
+
 }
