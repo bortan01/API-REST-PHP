d3d279e8642a110fa4be6457db115f56272b03a3
Boris Ricardo Miranda <brmiranda009@gmail.com>
2021-08-05 15:53:05 -0600

    ELIMINANDO EL CONF MODEL

--- application/models/Conf_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-class Conf_model extends CI_Model
-{
-
-    public $URL_SERVIDOR    = "http://localhost/API-REST-PHP/";
-    
-}
