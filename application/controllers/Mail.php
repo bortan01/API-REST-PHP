6ab2a6c05e8eadbf48aeda2887c0619fdbc7020f
JcMoz <jmozalfaro@gmail.com>
2021-10-16 22:30:12 -0600

    para seguridad lo borre

--- application/controllers/Mail.php
+++ application/controllers/Mail.php
@@ -18,11 +18,52 @@ class Mail extends REST_Controller
       $this->load->database();
       $this->load->model('Mail_model');
    }
-   //INSERTAR
+
+   //SOLO PARA UN DESTINATARIO
+   public function sendUno_post(){
+      $bodyHTML ='
+      <h2>Varios correos a la vez desde la BD</h2>
+      <br>
+      <br>
+      <br>
+      Mensaje final';
+  
+  
+      $enviado = $this->Mail_model->metEnviarUno("titulo, del correo","estoy probando","juan.moz@ues.edu.sv","asuntox",$bodyHTML);
+  
+      if($enviado){
+         // echo ("enviado");
+         $respuesta=array(
+				'err'=>FALSE,
+				'mensaje'=> $enviado['enviado']
+			);
+         $this->response($respuesta, REST_Controller::HTTP_OK);
+      }else{
+         // echo ("No se puede enviar el correo");
+         $respuesta=array(
+				'err'=>TRUE,
+				'mensaje'=>'No se puede enviar el correo'
+			);
+         $this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+
+      }
+         ///****este este es codigo de boris */
+     /* $data = $this->post();
+
+      $respuesta = $this->Mail_model->enviar($data);
+      if ($respuesta['err']) {
+         $this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+      } else {
+         $this->response($respuesta, REST_Controller::HTTP_OK);
+      }*/
+   }
+
+
+   //ESTA FUNCION ES PARA MAS DE UN DESTINATARIO
    public function send_post()
    {
       $bodyHTML ='
-      <h2>Hola,ZORRA FUNCIONA desde el hosting</h2>
+      <h2>Varios correos a la vez desde la BD</h2>
       <br>
       <br>
       <br>
@@ -35,7 +76,7 @@ class Mail extends REST_Controller
          // echo ("enviado");
          $respuesta=array(
 				'err'=>FALSE,
-				'mensaje'=>'Enviado'
+				'mensaje'=> $enviado['enviado']
 			);
          $this->response($respuesta, REST_Controller::HTTP_OK);
       }else{
