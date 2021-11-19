4fe0f32e9bb00ad1197efeb59cea1a11893244f7
Boris Ricardo Miranda <brmiranda009@gmail.com>
2021-11-18 20:58:45 -0600

    cambiando conexion en backup

--- application/models/Restore_model.php
+++ application/models/Restore_model.php
@@ -6,74 +6,79 @@ class Restore_model extends CI_Model
    function droptable()
    {
 
-      $connection = mysqli_connect('localhost', 'root', '', 'agencia');
+      $connection = mysqli_connect(
+         $this->Conf_model->HOST,
+         $this->Conf_model->USER,
+         $this->Conf_model->PASS,
+         $this->Conf_model->DBNM
+      );
 
-		$tablasExistentes = array();
-		$result = mysqli_query($connection, "SHOW TABLES");
-		while ($row = mysqli_fetch_row($result)) {
-			$tablasExistentes[] = $row[0];
-		}
+      $tablasExistentes = array();
+      $result = mysqli_query($connection, "SHOW TABLES");
+      while ($row = mysqli_fetch_row($result)) {
+         $tablasExistentes[] = $row[0];
+      }
 
       $tablasPorEliminar = [
-      "galeria",
-      "chat_record",
-      "detalle_servicio",
-      "servicios_adicionales",
-      "tipo_servicio",
-      "contacto",
-      "itinerario",
-      "sitio_turistico",
-      "tipo_sitio",
-      "reserva_tour",
-      "detalle_tour",
-      "tours_paquete",
-      "cotizar_tourpaquete",
-      "galeria_vehiculo",
-      "cotizarvehiculo",
-      "mantenimiento",
-      "reserva_vehiculo",
-      "detalle_serviciosvehiculo",
-      "detalle_vehiculo",
-      "vehiculo",
-      "modelo",
-      "transmisionvehiculo",
-      "usuarioRentaCar",
-      "rentaCar",
-      "categoria",
-      "marca_vehiculo",
-      "servicios_opc",
-      "bitacora",
-      "comision",
-      "detalle_encomienda",
-      "tarifa",
-      "unidades_medidas",
-      "producto",
-      "detalle_destino",
-      "detalle_envio",
-      "encomienda",
-      "municipio_envio",
-      "cita",
-      "opciones_respuestas",
-      "formulario_migratorio",
-      "pregunta",
-      "ramas_preguntas",
-      "info_adicional",
-      "general",
-      "cotizacion_vuelo",
-      "tipo_viaje",
-      "promocion_vuelo",
-      "aerolinea",
-      "alianza",
-      "tipo_clase",
-      "usuariorentacar",
-      "rentacar",
-      "usuario"];
+         "galeria",
+         "chat_record",
+         "detalle_servicio",
+         "servicios_adicionales",
+         "tipo_servicio",
+         "contacto",
+         "itinerario",
+         "sitio_turistico",
+         "tipo_sitio",
+         "reserva_tour",
+         "detalle_tour",
+         "tours_paquete",
+         "cotizar_tourpaquete",
+         "galeria_vehiculo",
+         "cotizarvehiculo",
+         "mantenimiento",
+         "reserva_vehiculo",
+         "detalle_serviciosvehiculo",
+         "detalle_vehiculo",
+         "vehiculo",
+         "modelo",
+         "transmisionvehiculo",
+         "usuarioRentaCar",
+         "rentaCar",
+         "categoria",
+         "marca_vehiculo",
+         "servicios_opc",
+         "bitacora",
+         "comision",
+         "detalle_encomienda",
+         "tarifa",
+         "unidades_medidas",
+         "producto",
+         "detalle_destino",
+         "detalle_envio",
+         "encomienda",
+         "municipio_envio",
+         "cita",
+         "opciones_respuestas",
+         "formulario_migratorio",
+         "pregunta",
+         "ramas_preguntas",
+         "info_adicional",
+         "general",
+         "cotizacion_vuelo",
+         "tipo_viaje",
+         "promocion_vuelo",
+         "aerolinea",
+         "alianza",
+         "tipo_clase",
+         "usuariorentacar",
+         "rentacar",
+         "usuario"
+      ];
 
       foreach ($tablasPorEliminar as $table) {
          if (in_array($table, $tablasExistentes)) {
             $this->dbforge->drop_table($table);
          }
       }
-
    }
 }
